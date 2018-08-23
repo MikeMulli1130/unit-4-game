5657b3820104c516e9ce75ef93099455ebe306bf
MikeMulli1130 <40740032+MikeMulli1130@users.noreply.github.com>
2018-08-23 18:19:55 -0400

    git commit - 619pm

---
+++ assets/javascript/game.js
@@ -0,0 +1,13 @@
+/* first choose a random number
+this is shown to the player at the beginning of the game */
+
+
+
+/* need four buttons - one for each crystal*/
+
+
+
+/*player will have to guess a number then a number is added to their guess*/
+
+
+var num = 0; 
