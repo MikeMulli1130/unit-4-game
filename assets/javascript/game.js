5f05eb99fb11def96963e3dbfa2e79731d335659
MikeMulli1130 <40740032+MikeMulli1130@users.noreply.github.com>
2018-08-25 07:56:56 -0400

    git commit -m "756am"

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,22 @@
-// GLOBAL VARIABLES (accessible by all functions)
-// ==================================================================================================
+//var list = ""; not sure what to do with this//
 
-// Computer selected solution will be held here.
+//computerNumber is the number under the buttons//
 var computerNumber = "";
+
+//crystalNumbers are the back end; var numberOne, etc., what to output when the buttons are pushed//
+var crystalNumbers = "";
+var numberOne, numberTwo, numberThree, numberFour;
+
+//value range for the outputs//
+
+numberOne >=1 && <= 49;
+numberTwo >=1 && <=49;
+numberThree >=1 && <=49;
+numberFour >=1 && <=49;
+
+//what the player selects//
+var playerCount =['']
+
 
 // Holds a list of guessed numbers 
 var guessedNumbers = [];
@@ -15,34 +29,38 @@ var winCounter = 0;
 var lossCounter = 0;
 var numGuesses = 9;
 
-/* first choose a random number this is shown to the player at the beginning of the game */
+/* random number this is shown to the player at the beginning of the game */
 
-chosenWord = wordsList[Math.floor(Math.random() * wordsList.length)];
+chosenNumber = computerNumber[Math.floor(Math.random() * list.length)];
 
-
+//random number for the crystals//
 
 randomNumber=crystalNumbers[Math.floor(Math.random()*crystalNumbers.length)];
 console.log(randomNumber);
 
-var playerCount =['']
-var numberOne, numberTwo, numberThree, numberFour;
-numberOne >=1 && <= 49;
 
-
-/* need four buttons - one for each "Crystal"; when one is clicked the number is shown*/
 /* the game keeps going until the player either goes over the original random number or*/
 /* or the player hits the number perfectly - if so, they win*/
 /* if the player goes OVER the original random number then they lose:*/
 
 
-document.getElementById("ruby").onclick = function(computerGuess) {
+document.getElementById("ruby").onclick = function(computerNumber) {
 console.log(numberOne)
 }
 
+document.getElementById("emerald").onclick = function(computerNumber) {
+  console.log(numberTwo)
+  }
 
-document.getElementById("emerald").onclick = numberTwo;
-document.getElementById("sapphire").onclick = numberThree;
-document.getElementById("topaz").onclick = numberFour;
+  document.getElementById("sapphire").onclick = function(computerNumber) {
+    console.log(numberThree)
+    }
+
+    document.getElementById("topaz").onclick = function(computerNumber) {
+      console.log(numberFour)
+      }
+      
+
 
 if {
     "numberOne + playerCount" = = = randomNumber
@@ -101,44 +119,16 @@ alert("Click another Crystal! You still have a chance!");
 }
 
 // startGame()
-// It's how we we will start and restart the game.
-// (Note: It's not being run here. Function declarations like this are made for future use.)
+
 function startGame() {
 
   // Reset the guesses back to 0.
   numGuesses = 9;
 
 
-  // CRITICAL LINE
-  // Here we *reset* the wrong guesses from the previous round.
+  //reset the wrong guesses from the previous round.
   wrongGuesses = [];
 
-  // Fill up the blanksAndSuccesses list with appropriate number of blanks.
-  // This is based on number of letters in solution.
-  for (var i = 0; i < numBlanks; i++) {
-    blanksAndSuccesses.push("_");
-  }
-
-  // Print the initial blanks in console.
-  console.log(blanksAndSuccesses);
-
-
-    // Log the current blanks and successes for testing.
-    console.log(blanksAndSuccesses);
-  }
-
-  // If the letter doesn't exist at all...
-  else {
-
-    // Then we add the letter to the list of wrong letters.
-    wrongGuesses.push(letter);
-
-    // We also subtract one of the guesses.
-    numGuesses--;
-
-  }
-
-}
 
 // roundComplete() function
 // Here we will have all of the code that needs to be run after each guess is made.
@@ -154,21 +144,14 @@ function roundComplete() {
   // Update the HTML to reflect the new number of guesses.
   document.getElementById("guesses-left").innerHTML = numGuesses;
 
-  // This will print the array of guesses and blanks onto the page.
-  document.getElementById("word-blanks").innerHTML = blanksAndSuccesses.join(" ");
-
   // This will print the wrong guesses onto the page.
   document.getElementById("wrong-guesses").innerHTML = wrongGuesses.join(" ");
 
-  // If our Word Guess string equals the solution.
-  // (meaning that we guessed all the letters to match the solution)...
-  if (lettersInChosenWord.toString() === blanksAndSuccesses.toString()) {
 
     // Add to the win counter
     winCounter++;
 
-    // Give the user an alert
-    alert("You win!");
+    
 
     // Update the win counter in the HTML
     document.getElementById("win-counter").innerHTML = winCounter;
